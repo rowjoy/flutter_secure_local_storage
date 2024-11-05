6bb3bbe8f6166eab0303a1be9af6853f1049c385
NecJoy <joy@primotechltd.com>
2024-11-05 18:04:35 +0600

    Add some information

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,18 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, avoid_print
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter_local_storage/flutter_local_storage.dart';
+
+FlutterLocalStorage box = FlutterLocalStorage();
+
+void main() async{
+ WidgetsFlutterBinding.ensureInitialized();
+  /// your_secret_key is encript your local store data .
+  await box.initFlutterLocalStorage(secretKey: "your_secret_key");
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,24 +23,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -39,32 +35,70 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+
 class _MyHomePageState extends State<MyHomePage> {
+ 
+ Map<String , dynamic> mapValue = {
+
+  "user": {
+    "id": 1,
+    "name": "Alice",
+    "email": "alice@example.com",
+    "address": {
+      "street": "123 Main St",
+      "city": "Wonderland",
+      "zipcode": "12345"
+    },
+    "orders": [
+      {
+        "id": 101,
+        "product": "Book",
+        "quantity": 2,
+        "price": 15.5
+      },
+      {
+        "id": 102,
+        "product": "Pen",
+        "quantity": 5,
+        "price": 2.5
+      }
+    ]
+  }
+};
+
+
+
+
   int _counter = 0;
+  String keyNumber = "keyNumber";
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+
       _counter++;
+      box.write(keyNumber, "Jamirul islam");
+
+  // Write encrypted data
+   box.write('username', 'Alice');
+   box.write('age', 30);
+   box.write('age1', 30.001);
+   box.write('createdAt', DateTime.now());
+   box.write("MapValue", mapValue);
+
+    // Read and decrypt data
+  print(box.read('username')); // Should print 'Alice'
+  print(box.read('age')); 
+  // Should print 30
+  print(box.read('age1'));
+  print(box.read('createdAt'));
+  print(box.read("MapValue")["user"]["email"]);
     });
   }
 
@@ -84,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(box.read(keyNumber).toString() ?? ""),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -103,8 +137,15 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
+          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ElevatedButton(
+              onPressed: (){
+                // Navigator.push(context, MaterialPageRoute(builder: (context)=> const NextScreen()));
+              }, 
+              child: Text("Cleck me"),
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
@@ -117,9 +158,11 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
--- lib/flutter_local_storage.dart
+++ lib/flutter_local_storage.dart
@@ -1,7 +1,4 @@
-library flutter_local_storage;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library flutter_local_storage;
+export 'src/internal_database.dart';
+export 'src/internal_database_io.dart';
